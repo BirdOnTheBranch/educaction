adc714113a743afdbe32240f9a4847ee7f676d33
BirdOnTheBranch <planelles.luis@gmail.com>
2020-09-02 20:02:25 +0200

    Created a new mixim customized.

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,60 @@
+from django.urls import reverse_lazy
+from django.views.generic.list import ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
 from django.shortcuts import render
 
-# Create your views here.
+from .models import Course
+
+
+class ManageCourseListView(ListView):
+    model = Course
+    template_name = 'courses/manage/course/list.html'
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerMixin(object):
+
+    def get_queryset(self):
+        qs = super().get_queryset()
+        return qs.filter(owner=self.request.user)
+
+
+class OwnerEditMixin(object):
+
+    def get_queryset(self, form):
+        form.instance.owner = self.request.user
+        return super().form_valid(form)
+
+
+class OwnerCourseMixin(OwnerMixin, LoginRequiredMixin):
+    model = Course
+    fields = ['subject','title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+
+
+class OwnerCourseEditMixin(OwnerCourseMixin, OwnerEditMixin):
+    fields = ['subject','title', 'slug', 'overview']
+    success_url = reverse_lazy('manage_course_list')
+    template_name = 'courses/manage/course/form.html'
+
+
+class ManageCourseListView(OwnerCourseMixin, ListView):
+    template_name = 'courses/manage/course/list.html'
+
+
+class CourseCreateView(PermissionRequiredMixin, OwnerCourseEditMixin, CreateView):
+    Permission_required = 'courses.add_course'
+
+
+class CourseUpdateView(PermissionRequiredMixin, OwnerCourseEditMixin, UpdateView):
+    Permission_required = 'courses.add_course'
+
+
+class CourseDeleteView(PermissionRequiredMixin, OwnerCourseMixin, DeleteView):
+    template_name = 'courses/manage/courses/delete.html'
+    success_url = reverse_lazy('manage_course_list')
+    Permission_required = 'courses.add_course'
