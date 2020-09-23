2985aa82c6b92674c6ad62e7e6f79cc453637eda
BirdOnTheBranch <planelles.luis@gmail.com>
2020-09-24 00:23:24 +0200

    Configuration files prepared and divided for the production phase, and the application has started to optimize to add nginx.

--- educaction/settings/base.py
+++ educaction/settings/base.py
@@ -13,16 +13,13 @@ import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve(strict=True).parent.parent
+BASE_DIR = Path(__file__).resolve(strict=True).parent.parent.parent
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'q9f$(#qt7j&27t)h^m-q!d8$*%o-qnc6w(s$6ps_r-7*^7t%or'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
 ALLOWED_HOSTS = []
 
@@ -73,14 +70,6 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'educaction.wsgi.application'
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
---
+++ educaction/settings/pro.py
@@ -0,0 +1,23 @@
+from .base import *
+
+
+
+# SECURITY WARNING: don't run with debug turned on in production!
+DEBUG = False
+
+
+ADMINS = ('wane', 'wane@hotmail.com')
+
+ALLOWED_HOSTS = ['*']
+
+# Database
+# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
+
+DATABASES = {
+   'default': {
+       'ENGINE': 'django.db.backends.postgresql',
+       'NAME': 'educa',
+       'USER': 'educa',
+       'PASSWORD': '222',
+   }
+}
